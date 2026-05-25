6e7dcae896ba784ce3ad290d594b398fce01997b
Manuel <manuelverko95@gmail.com>
2026-05-25 14:51:56 -0400

    feat:primer test

---
+++ src/test/java/PrimerTest.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class PrimerTest {
+
+    WebDriver driver;
+    WebDriverWait wait;
+    @BeforeMethod
+    void setup(){
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+    }
+
+    @AfterMethod
+    void tearDown(){
+        driver.quit();
+    }
+
+    @Test
+    void creteTestDriver(){
+        driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        var barraBusqueda = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("q")));
+        if (barraBusqueda.isDisplayed()){
+            barraBusqueda.sendKeys("Selenium");
+            barraBusqueda.submit();
+        }
+    }
+}
